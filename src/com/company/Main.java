a277ebf7a3663236cd7961fd80f40d555773eb06
Marina <marina-ageeva@mail.ru>
2017-06-09 02:41:49 +0300

    gh ghg gh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// сюда пишешь задание ok/ потом жмешь
-        //dkjgldfh;lskdfj
-          //      dh
-        //fg
+	// ghfgh
     }
 }
